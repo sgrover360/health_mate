a58ab5bec03626cc02909840ba89c03baea45d69
Ejaz-dev <ejazkhan0123@gmail.com>
2023-11-17 22:50:20 -0330

    Implemented authentication gateway functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,30 @@
-import 'package:flutter/material.dart';
+import 'package:health_mate/views/auth_gate.dart';
 import 'package:health_mate/views/logo_animation.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  // Ensure Flutter is initialized.
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MaterialApp(
     theme: ThemeData(scaffoldBackgroundColor: Colors.red.shade50),
     home: const Scaffold(
       body: LogoAnimation(),
     ),
   ));
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        home: AuthGate(),
+      );
 }
